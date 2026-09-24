cf10b0a6e5e92a6e8f6c51797a07d64a5bf04020
Krunalsinh <krunalsinh7718@gmail.com>
2026-09-24 13:23:03 +0530

    chnages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import glsl from 'vite-plugin-glsl'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), glsl()],
+  build:
+    {
+        target: 'esnext',
+         chunkSizeWarningLimit: 2000 
+    },
 })
